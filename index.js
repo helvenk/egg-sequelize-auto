d3582fd8ce8df2bb1bd6719f29829e6f3e3534c0
lk <lkspc@qq.com>
2017-09-27 18:36:29 +0800

    x

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: lkspc 
+ * @Date: 2017-09-27 16:19:13 
+ * @Last Modified by: lkspc
+ * @Last Modified time: 2017-09-27 17:49:27
+ */
+
+'use strict';
+
+module.exports = require('./lib');
